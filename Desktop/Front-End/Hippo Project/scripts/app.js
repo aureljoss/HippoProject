56175f7049aa634acd179539e7fffe0e7e9d10a6
aureljoss <josserand.n.aurelie@gmail.com>
2022-08-17 21:18:45 -0400

    Hippo svg animated with GSAP

---
+++ Desktop/Front-End/Hippo Project/scripts/app.js
@@ -0,0 +1,21 @@
+// ------ Hover Animation ----- //
+
+const mouthOpen= gsap.timeline ({repeat: -1});
+const easeType= Power2.easeOut;
+const mouthSpeed= 1;
+mouthOpen.to('.mouth-back',{duration:mouthSpeed, ease: easeType, y:-70},0);
+mouthOpen.to('.tongue',{duration:mouthSpeed * 1.5, ease: easeType, y:-70,},0);
+mouthOpen.to('.teeth',{duration:mouthSpeed, ease: easeType, y:-70, scaleY: 1.2},0);
+mouthOpen.to('.freckles', {duration: mouthSpeed, ease: easeType, y: -8},0);
+mouthOpen.to('.ears', {duration: mouthSpeed, ease: easeType, y:10},0);
+mouthOpen.to('.eye-right', {duration: mouthSpeed, ease: easeType, x:-2},0);
+mouthOpen.to('.eye-left', {duration: mouthSpeed, ease: easeType, x:2},0);
+mouthOpen.to('.eyes', {duration: mouthSpeed, ease: easeType, y:2},0);
+mouthOpen.to('.nostrils', {duration: mouthSpeed, ease: easeType, y: -2},0);
+
+// ------ Mouse Hover -----//
+
+const button = document.querySelector('button');
+
+button.addEventListener{'mouseenter', enterButton};
+button.addEventListener{'mouseleave', leaveButton};
